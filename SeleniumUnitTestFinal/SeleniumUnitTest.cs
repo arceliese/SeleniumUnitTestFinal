bd7998a89dc0ff4abaa7e52405b640d44b0eb3c7
arceliese <arceliese@gmail.com>
2016-01-21 14:53:49 +1300

    Test Commit 8

--- SeleniumUnitTestFinal/SeleniumUnitTest.cs
+++ SeleniumUnitTestFinal/SeleniumUnitTest.cs
@@ -49,7 +49,7 @@ namespace NUnitSelenium
             //Step e
             driver.Quit();
             Console.WriteLine("===============================================");
-            Console.WriteLine("|    T E S T   C O M M I T  7                  |");
+            Console.WriteLine("|    T E S T   C O M M I T  8                  |");
             Console.WriteLine("===============================================");
         }
 
